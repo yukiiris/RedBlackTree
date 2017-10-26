796aea336536489ac76af4bc544ea44339511ea1
moltres <25852469@qq.com>
2017-10-26 23:07:25 +0800

    没写多少

---
+++ src/BlackRedTree.java
@@ -0,0 +1,26 @@
+public class BlackRedTree<T> {
+
+    Node<T> root;
+    private class Node<T>
+    {
+        private T value;
+        private Node<T> left;
+        private Node<T> right;
+        private boolean isRed;
+
+        public Node()
+        {
+            isRed = false;
+        }
+    }
+
+    public BlackRedTree()
+    {
+        root = new Node<>();
+    }
+
+    public get()
+    {
+
+    }
+}
